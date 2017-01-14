a0ff5ad81606b041b7b7f08815f292473d9ed458
Cesar Alex Diaz <calexdiaz143@gmail.com>
2017-01-14 05:28:58 -0800

    Updates lol

--- processing.php
+++ processing.php
@@ -1,4 +1,20 @@
 <?php
+	include('challonge.class.php');
+	date_default_timezone_set('America/Los_Angeles');
+
+
+	$html = file_get_contents('http://ocsmash.weebly.com/the-ladder-current.html');
+	$dom = new DOMDocument;
+	$dom->loadHTML($html);
+
+	$items = $dom->getElementsByTagName('center');
+	$players =array();
+
+	for ($i = 6; $i < $items->length; $i = 4 + $i){
+		$name = (string)$items->item($i)->nodeValue;
+		$players[$name] = $items->item($i + 1)->nodeValue;
+	}
+
 	$tagListString = $_POST['tagList'];
 	$tagList = explode(" ", $tagListString);
 	//Get trueskill of each person and attach to person in array
@@ -12,6 +28,22 @@
 	ksort($seedingList);
 
 	//Send sorted list to challonge
+	$c = new ChallongeAPI(DpYyhqBBN7gJX0LFMx8kgggYh51IZkDUnnP8nJ2p);
+
+	$tournamentName = trim($_POST['tournamentName']); ///change this line for the post
+	$tourneyNum = $tournamentName[strlen(tournamentName)-1] . $tournamentName[strlen(tournamentName)-2];
+
+	$date = date('m/d/Y h:i:s a', time());
+
+	$params = array(
+  		"tournament[name]" => $tournamentName,//fix this line lol
+  		"tournament[tournament_type]" => "double elimination",
+  		"tournament[url]" => "ZsmashBi" . $tourneyNum,
+  		"tournament[description]" => "Challonge api is pretty dope, also Alex and Bill are GOATS",
+  		"tournament[start_at]"  =>  $date
+  	);
+	$tournament = $c->makeCall("tournaments", $params, "post");
+	$tournament = $c->createTournament($params);
 
 	//Get challonge link back
 
